cf55d28b99b1958165d19b1a0122b3776364281a
shaho <shaho.parvini@gmail.com>
2025-11-04 16:36:33 +0330

    using std::plus

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
 auto main() -> int {
-    std::cout << "Hello Again C++!\n";
+    std::cout << std::plus<int>{}(1, 1.23) << '\n'; 
+    std::cout << std::plus<>{}(1, 1.23) << '\n';
 }
